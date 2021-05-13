abf6d995372d1b4adb703c835a5b81f9764d6187
Hosuqa <43502805+Hosuqa@users.noreply.github.com>
2021-05-13 12:18:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+//menu mobilne
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
